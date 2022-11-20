27ce418633f21389069375d4a05710294ed669ca
jaemin <anjm1020@gmail.com>
2022-11-20 18:22:37 +0900

    Feat : Problem 2

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,34 @@
 package onboarding;
 
+import java.util.Stack;
+import java.util.stream.Collectors;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        Stack stack = generateResultStack(cryptogram);
+        return makeAnswerFromStack(stack);
+    }
+
+    private static String makeAnswerFromStack(Stack stack) {
+        String res = "";
+        while (!stack.isEmpty()) {
+            res = stack.pop() + res;
+        }
+        return res;
+    }
+
+
+    public static Stack<Character> generateResultStack(String target) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : target.toCharArray()) {
+            if(stack.empty()) stack.push(c);
+            else {
+                char top = stack.peek();
+                if(top == c) stack.pop();
+                else stack.push(c);
+            }
+        }
+        return stack;
     }
 }
