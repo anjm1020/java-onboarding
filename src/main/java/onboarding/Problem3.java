a43fc657c68acab8bb2997ca51b3fd2e58bd1599
jaemin <anjm1020@gmail.com>
2022-11-21 12:29:38 +0900

    Feat : Problem 3

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,22 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+
+        for (int i = 1; i <= number; i++) {
+            answer += count(i);
+        }
+
         return answer;
     }
+
+    public static int count(int number) {
+        int cnt = 0, digit;
+        while (number != 0) {
+            digit = number % 10;
+            cnt += digit != 0 && digit % 3 == 0 ? 1 : 0;
+            number /= 10;
+        }
+        return cnt;
+    }
+
 }
