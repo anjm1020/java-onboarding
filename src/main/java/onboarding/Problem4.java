59d3b0674e5161dd9b61549c4a52e9c96b412db9
jaemin <anjm1020@gmail.com>
2022-11-21 12:29:51 +0900

    Feat : Problem 4

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,23 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+        StringBuilder sb = new StringBuilder();
+        for (char c : word.toCharArray()) {
+            sb.append(convert(c));
+        }
+        answer = sb.toString();
         return answer;
     }
+
+    public static char convert(char c) {
+        char res, start;
+        int diff;
+        if(!Character.isLetter(c)) return res = ' ';
+        start = Character.isUpperCase(c) ? 'A' : 'a';
+        diff = 25 - (c - start);
+        res = (char) (start + diff);
+        return res;
+    }
+
+    // 더해서 25 만들기
 }
